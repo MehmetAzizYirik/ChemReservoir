70f009b463b676251949b81aac117d8f44d6dfcc
mehmetazizyirik <mehmetazizyirik@imada.sdu.dk>
2025-05-26 02:22:30 +0300

    Deleted comments from tests.py file.

--- scripts/tests.py
+++ scripts/tests.py
@@ -3,24 +3,24 @@ from scripts.chemReservoir import chemReservoir
 
 
 loggerShortMemory = setup_logger("shortMemoryTaskLoggerFile")
-shortMemoryTask = chemReservoir(1, loggerShortMemory, memoryTask="short", runTime=10, repetition=2, networkOptMaxTime=200) #run time 50, max time 500
+shortMemoryTask = chemReservoir(1, loggerShortMemory, memoryTask="short", runTime=10, repetition=2, networkOptMaxTime=200)
 shortMemoryTask.INT_BOUNDS = {
             'nodeValues': (50, 300),
             'moleculeInflow': (50, 200),
             'chordDistance': (5, 25),
             'chordStep': (5, 25)
         }
-shortMemoryResult=shortMemoryTask.run(max_time=1000) #10000
+shortMemoryResult=shortMemoryTask.run(max_time=1000)
 print("Short memory results", shortMemoryResult)
 
 loggerLongMemory = setup_logger("longMemoryTaskLoggerFile")
-longMemoryTask = chemReservoir(1, loggerLongMemory, memoryTask="long", tau=6, runTime=50, repetition=2, networkOptMaxTime=200) #run time 50, max time 500
+longMemoryTask = chemReservoir(1, loggerLongMemory, memoryTask="long", tau=6, runTime=50, repetition=2, networkOptMaxTime=200)
 longMemoryTask.INT_BOUNDS = {
             'nodeValues': (50, 300),
             'moleculeInflow': (50, 200),
             'chordDistance': (5, 25),
             'chordStep': (5, 25)
         }
-longMemoryResult=longMemoryTask.run(max_time=500) #10000
+longMemoryResult=longMemoryTask.run(max_time=500)
 print("Long memory results", longMemoryResult)
 
