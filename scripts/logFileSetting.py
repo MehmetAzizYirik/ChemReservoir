873cec733eb2ecacf4d18f410a71d9cc155500b2
mehmetazizyirik <mehmetazizyirik@imada.sdu.dk>
2025-05-27 12:32:52 +0300

    Updated log file setting file.

--- scripts/logFileSetting.py
+++ scripts/logFileSetting.py
@@ -1,27 +1,27 @@
 import logging
 import os
 
-def setup_logger(task_name, log_dir="results"):
+def setupLogger(fileName, dir="results"):
     """
-    Setting the loger file for the intermediate steos of the chemReservoir tool.
+    Setting the loger file for the intermediate steps of the chemReservoir tool.
 
     Args:
-        task_name (string): title of the log file
-        log_dir (string): where the file is stored
+        fileName (string): title of the log file
+        dir (string): where the file is stored
 
     Returns:
         logging.Logger: logger file for intermediate steps.
     """
-    os.makedirs(log_dir, exist_ok=True)
+    os.makedirs(dir, exist_ok=True)
 
-    logger = logging.getLogger(task_name)
+    logger = logging.getLogger(fileName)
     logger.setLevel(logging.INFO)
 
     if not logger.handlers:
-        log_path = os.path.join(log_dir, f"{task_name}.log")
-        file_handler = logging.FileHandler(log_path)
+        logPath = os.path.join(dir, f"{fileName}.log")
+        fileHandler = logging.FileHandler(logPath)
         formatter = logging.Formatter("%(levelname)s - %(message)s")
-        file_handler.setFormatter(formatter)
-        logger.addHandler(file_handler)
+        fileHandler.setFormatter(formatter)
+        logger.addHandler(fileHandler)
 
     return logger
